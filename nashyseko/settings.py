0ad426d26232d76ad3fe34480e935b4c3153fb30
Rocket8900 <nashwyn02@gmail.com>
2022-03-15 20:38:28 +0800

    changes to some settings

--- nashyseko/settings.py
+++ nashyseko/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'nashyseko.urls'
@@ -152,9 +152,9 @@ LEAFLET_CONFIG = {
     'ATTRIBUTION_PREFIX': 'Inspired by Life in GIS'
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 BASE_COUNTRY = "SG"
 
