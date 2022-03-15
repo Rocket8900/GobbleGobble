85226944072986609c15342884ab54406632f6ba
Rocket8900 <nashwyn02@gmail.com>
2022-03-15 21:51:28 +0800

    changed debug

--- nashyseko/settings.py
+++ nashyseko/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-de^a9nt!y+xav@_hq@*xb^eo^t#(c+ozl_4t85$tic#o#3pg2g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hangrysloth.herokuapp.com', '127.0.0.1']
 
