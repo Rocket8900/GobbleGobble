87e978b923c4e0a96a8249b01a4cab64c078e529
Rocket8900 <nashwyn02@gmail.com>
2022-05-12 21:16:59 +0800

    changed load env settings

--- nashyseko/settings.py
+++ nashyseko/settings.py
@@ -13,13 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-load_dotenv() 
+load_dotenv(find_dotenv()) 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
