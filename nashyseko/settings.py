1f43059350a4c71db17bb85baf114c6369af7a26
Rocket8900 <nashwyn02@gmail.com>
2022-05-12 21:21:54 +0800

    added secret key to heroku

--- nashyseko/settings.py
+++ nashyseko/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # AWS S3 SETTINGS
 AWS_ACCESS_KEY_ID = os.getenv("SECRET_AWS_ACCESS_KEY_ID")
