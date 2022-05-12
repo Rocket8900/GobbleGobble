783614ae8a5db3030d7c782500a756721edfc413
Rocket8900 <nashwyn02@gmail.com>
2022-05-12 21:35:13 +0800

    moved back into setting

--- nashyseko/settings.py
+++ nashyseko/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-de^a9nt!y+xav@_hq@*xb^eo^t#(c+ozl_4t85$tic#o#3pg2g'
 
 # AWS S3 SETTINGS
 AWS_ACCESS_KEY_ID = os.getenv("SECRET_AWS_ACCESS_KEY_ID")
