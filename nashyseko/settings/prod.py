be55febd251053d33cc219d3bfbf9fe058d2c24e
Rocket8900 <nashwyn02@gmail.com>
2022-05-17 01:24:36 +0800

    made some changes to database

--- nashyseko/settings/prod.py
+++ nashyseko/settings/prod.py
@@ -15,31 +15,28 @@ ALLOWED_HOSTS = [
 
 # Amazon S3 Settings
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
+AWS_ACCESS_KEY_ID = 'AKIA347MMUKRYJDRPN56'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'hangrysloth'
+AWS_S3_CUSTOM_DOMAIN = 'hangrysloth.s3.amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'
-
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-
 AWS_LOCATION = 'static'
-
 AWS_QUERYSTRING_AUTH = False
-
-AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
-
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin':'*',
+}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# AWS_S3_REGION = 'ap-southeast-1'
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+CKEDITOR_UPLOAD_PATH= f'https://{AWS_S3_CUSTOM_DOMAIN}/uploads/'
 
 
 # Heroku Logging
