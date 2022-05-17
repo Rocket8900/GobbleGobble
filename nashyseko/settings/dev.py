0cf40084c65ee9d541ac4b63da477003450e38bd
Rocket8900 <nashwyn02@gmail.com>
2022-05-17 17:58:32 +0800

    added environ variables

--- nashyseko/settings/dev.py
+++ nashyseko/settings/dev.py
@@ -5,11 +5,3 @@ SECRET_KEY = 'django-insecure-de^a9nt!y+xav@_hq@*xb^eo^t#(c+ozl_4t85$tic#o#3pg2g
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
