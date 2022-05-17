2604331d02ca07b12815152493ffaaea2d75848b
Rocket8900 <nashwyn02@gmail.com>
2022-05-17 18:08:10 +0800

    added environ variables w

--- nashyseko/settings/prod.py
+++ nashyseko/settings/prod.py
@@ -3,7 +3,7 @@ from decouple import config
 
 from .base import *
 
-SECRET_KEY = 'django-insecure-de^a9nt!y+xav@_hq@*xb^eo^t#(c+ozl_4t85$tic#o#3pg2g'
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
@@ -13,30 +13,30 @@ ALLOWED_HOSTS = [
 ]
 
 
-# Amazon S3 Settings
+Amazon S3 Settings
 
-# AWS_ACCESS_KEY_ID = 'AKIA347MMUKRYJDRPN56'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'hangrysloth'
-# AWS_S3_CUSTOM_DOMAIN = 'hangrysloth.s3.amazonaws.com'
-# AWS_DEFAULT_ACL = 'public-read'
-# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-# AWS_LOCATION = 'static'
-# AWS_QUERYSTRING_AUTH = False
-# AWS_HEADERS = {
-#     'Access-Control-Allow-Origin':'*',
-# }
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIA347MMUKRYJDRPN56'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'hangrysloth'
+AWS_S3_CUSTOM_DOMAIN = 'hangrysloth.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_LOCATION = 'static'
+AWS_QUERYSTRING_AUTH = False
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin':'*',
+}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-# # AWS_S3_REGION = 'ap-southeast-1'
-# # AWS_S3_SIGNATURE_VERSION = 's3v4'
+# AWS_S3_REGION = 'ap-southeast-1'
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-# CKEDITOR_UPLOAD_PATH= f'https://{AWS_S3_CUSTOM_DOMAIN}/uploads/'
+CKEDITOR_UPLOAD_PATH= f'https://{AWS_S3_CUSTOM_DOMAIN}/uploads/'
 
 
 # Heroku Logging
