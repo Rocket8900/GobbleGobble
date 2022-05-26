c6110134b204827c5693a3d08b0187c6ab13ed91
Rocket8900 <nashwyn02@gmail.com>
2022-05-26 23:25:54 +0800

    fixed bugs removed both lists

--- nashyseko/settings/dev.py
+++ nashyseko/settings/dev.py
@@ -5,3 +5,16 @@ SECRET_KEY = 'django-insecure-de^a9nt!y+xav@_hq@*xb^eo^t#(c+ozl_4t85$tic#o#3pg2g
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+STATIC_URL = 'static/'
+
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
